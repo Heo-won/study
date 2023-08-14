7325256344d77979ca415923a338848fc1e756d4
WON <[hwon466@gmail.com]>
2023-08-15 00:14:44 +0900

    study

---
+++ week_1/01.js
@@ -0,0 +1,20 @@
+//변수를 선언할 수 있는 3가지 방법 : var, let, const
+//1.var
+var myVar = "Hello world"
+var myVar = "Test 1"
+myVar = "GoodBye"
+console.log(myVar)
+
+//2.let
+let myLet = "Hello world1"
+// let myLet = "Test 2"
+myLet = "GoodBye"
+console.log(myLet)
+
+const myConst = "Hello world2"
+// const myConst = "Test 2"
+myConst = "GoodBye 2"
+
+console.log(myConst)
+
+
